b3e96331757cebe44ccb4433eff2bb7ab59fea58
Irene Han <irenehan@stanford.edu>
2019-11-20 23:22:08 -0800

    add more events

--- constants/articles.js
+++ constants/articles.js
@@ -5,7 +5,6 @@ export default [
     activity: 'active now',
     attending: '2 attending',
     image: 'https://images.unsplash.com/photo-1516559828984-fb3b99548b21?ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80',
-    cta: 'View article',
     horizontal: true
   },
   {
@@ -14,7 +13,6 @@ export default [
     activity: 'active now',
     attending: '2 attending',
     image: 'https://images.unsplash.com/photo-1519368358672-25b03afee3bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2004&q=80',
-    cta: 'View article',
     horizontal: true
   },
   {
@@ -23,7 +21,6 @@ export default [
     activity: 'active now',
     attending: '2 attending',
     image: 'https://images.unsplash.com/photo-1500522144261-ea64433bbe27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80',
-    cta: 'View article',
     horizontal: true
   },
   {
@@ -32,7 +29,6 @@ export default [
     activity: 'active now',
     attending: '2 attending',
     image: 'https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1326&q=80',
-    cta: 'View article',
     horizontal: true
   },
   {
@@ -41,7 +37,30 @@ export default [
     activity: 'active now',
     attending: '2 attending',
     image: 'https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?fit=crop&w=1947&q=80',
-    cta: 'View article',
+    horizontal: true
+  },
+  {
+    title: 'Event 6 …',
+    distance: '0.2 mi',
+    activity: 'active now',
+    attending: '2 attending',
+    image: 'https://images.unsplash.com/photo-1516559828984-fb3b99548b21?ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80',
+    horizontal: true
+  },
+  {
+    title: 'Event 7 …',
+    distance: '0.2 mi',
+    activity: 'active now',
+    attending: '2 attending',
+    image: 'https://images.unsplash.com/photo-1519368358672-25b03afee3bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2004&q=80',
+    horizontal: true
+  },
+  {
+    title: 'Event 8 …',
+    distance: '0.2 mi',
+    activity: 'active now',
+    attending: '2 attending',
+    image: 'https://images.unsplash.com/photo-1500522144261-ea64433bbe27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80',
     horizontal: true
   },
 ];
