a1f92063728c2a9d9678abe91ca030544764edde
Irene Han <irenehan@stanford.edu>
2019-11-27 11:14:55 -0800

    expandable (todo: figure out how to set height)

--- components/EventCard.js
+++ components/EventCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { withNavigation } from 'react-navigation';
 import PropTypes from 'prop-types';
-import { StyleSheet, Dimensions, Image, View } from 'react-native';
+import { StyleSheet, Dimensions, Image, View, Animated } from 'react-native';
 import { Block, Text, theme } from 'galio-framework';
 import { Colors, Metrics } from '../Themes';
 import { Card, Title, Subheading, Paragraph, Button, Avatar } from 'react-native-paper';
@@ -12,77 +12,106 @@ import Icon from './Icon';
 
 class EventCard extends React.Component {
   state = {
-    expanded: this.props.expanded || false,
-  };
+    expanded: true,
+    animation: new Animated.Value()
+  }
 
   _handlePress = () => {
-    this.props.onPress && this.props.onPress();
+    let initialValue = this.state.expanded? this.state.maxHeight + this.state.minHeight : this.state.minHeight;
+    let finalValue = this.state.expanded? this.state.minHeight : this.state.maxHeight + this.state.minHeight;
 
-    if (this.props.expanded === undefined) {
-      // Only update state of the `expanded` prop was not passed
-      // If it was passed, the component will act as a controlled component
-      this.setState(state => ({
-        expanded: !state.expanded,
-      }));
-    }
-  };
+    this.setState({
+      expanded: !this.state.expanded
+    });
+
+    this.state.animation.setValue(initialValue);
+    Animated.spring(
+        this.state.animation,
+        {
+            toValue: finalValue
+        }
+    ).start();
+  }
+
+  _setMaxHeight = (event) =>
+    this.setState({
+        maxHeight: event.nativeEvent.layout.height
+  });
+
+  _setMinHeight = (event) =>
+    this.setState({
+        minHeight: event.nativeEvent.layout.height
+  });
 
   render() {
     const { item } = this.props;
 
     return (
-      <View flex style={styles.container}>
-        <Avatar.Image size={45} source={{uri: item.image}} style={styles.img}/>
-        <View>
-          <View style={styles.header}>
-            <Title style={styles.title} >{item.title}</Title>
+      <Animated.View style={styles.container}>
+        <View flex style={styles.top} onLayout={this._setMinHeight.bind(this)}>
+          <Avatar.Image size={45} source={{uri: item.image}} style={styles.img}/>
+          <View>
+            <View style={styles.header}>
+              <Title style={styles.title} >{item.title}</Title>
+              <Icon
+                family="feather"
+                size={15}
+                name="bookmark"
+                color= {Colors.orange}
+                onPress={() => {console.log('Pressed')}}
+              />
+            </View>
+            <Text style={styles.text}>{item.description}</Text>
+          </View>
+          <View flex column>
             <Icon
               family="feather"
-              size={15}
-              name="bookmark"
-              color= {Colors.orange}
-              onPress={() => {console.log('Pressed')}}
+              size={25}
+              name= {this.state.expanded ? "chevron-up" : "chevron-down"}
+              color= {fidoTheme.COLORS.GREY}
+              style={{alignSelf: "flex-end"}}
+              onPress={this._handlePress}
             />
           </View>
-          <Text style={styles.text}>{item.description}</Text>
+        </View>
+        <View flex style={styles.bottom} onLayout={this._setMaxHeight.bind(this)}>
           <View flex style={styles.info} >
             <Text style={styles.text} >When: {item.activity}</Text>
             <Text style={styles.text} >Where: {item.location}</Text>
             <Text style={styles.text} >Who: {item.attending}</Text>
           </View>
+          <View style={styles.buttonContainer}>
+            <Button
+              mode="contained"
+              compact={true}
+              uppercase={false}
+              color={Colors.orange}
+              labelStyle={styles.buttonText}
+              onPress={() => {console.log('Pressed')}}
+            >
+              Navigate
+            </Button>
+          </View>
         </View>
-        <View style={styles.buttonContainer}>
-          <Icon
-            family="feather"
-            size={25}
-            name= {this.state.expanded ? "chevron-up" : "chevron-down"}
-            color= {fidoTheme.COLORS.GREY}
-            onPress={() => {console.log('Pressed')}}
-          />
-          <Button
-            mode="contained"
-            compact={true}
-            uppercase={false}
-            color={Colors.orange}
-            labelStyle={styles.buttonText}
-            onPress={() => {console.log('Pressed')}}
-          >
-            Navigate
-          </Button>
-        </View>
-      </View>
+      </Animated.View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    padding: 10,
-    paddingBottom: 20,
+    flex: 1,
     borderColor: fidoTheme.COLORS.BORDER,
     borderBottomWidth: 1,
+    paddingBottom: 20,
+    padding: 10,
+  },
+  top: {
+    flexDirection: "row",
+  },
+  bottom: {
+    flexDirection: "row",
+    marginLeft: 60,
   },
   header: {
     flexDirection: "row",
@@ -104,14 +133,8 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   buttonContainer: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "space-between",
-    alignItems: "flex-end",
-  },
-  expandableButtons: {
-    alignItems: "flex-end",
     flexDirection: "row",
+    alignItems: "flex-end",
   },
   buttonText: {
     color: "white",
