7a7a9399b71c955f089ab355e74234223d0e8bd9
Irene Han <irenehan@stanford.edu>
2019-12-01 13:50:50 -0800

    styling fix on my events and bookmarks

--- screens/MyEvents.js
+++ screens/MyEvents.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { BookmarkCard } from '../components'
+import { BookmarkCard, Person } from '../components'
 import articles from '../constants/articles';
 import { StyleSheet, Text, View, ScrollView, Dimensions, Image } from 'react-native';
 import { Colors, Metrics } from '../Themes';
 import { List, Checkbox } from 'react-native-paper';
-import { FloatingActionButton, ListItem, EventCard } from '../components';
 import fidoTheme from "../constants/Theme";
 import { Block, theme } from 'galio-framework';
 import { Card, Title, Subheading, Paragraph, Button, Avatar, TextInput } from 'react-native-paper';
@@ -21,37 +20,6 @@ var Jack = {
   icon: true,
 };
 
-
-// PERSON COMPONENT (used in MyEvents)
-class Person extends React.Component {
-  render() {
-    const { person } = this.props;
-    return (
-      <View flex style={styles.container}>
-        <Avatar.Image size={45} source={{uri: person.image}} style={styles.img}/>
-        <View>
-          {person.icon ?
-            <Title style={styles.title} >{person.name}</Title>
-          :
-            <Title style={styles.message} >{person.name}</Title>}
-        </View>
-        <View style={styles.buttonContainer}>
-        {person.icon ?
-          <Icon
-            family="feather"
-            size={25}
-            name="user-plus"
-            color= {fidoTheme.COLORS.GREY}
-            style= {{marginRight: 20}}
-            onPress={() => {console.log("PRESSED")}}
-          />
-        : <View></View>}
-        </View>
-      </View>
-    );
-  }
-}
-
 var sampleMessage = {
   image: 'https://images.unsplash.com/photo-1500522144261-ea64433bbe27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80',
   name: "Are you guys still there? I'm omw!",
@@ -62,18 +30,14 @@ var sampleMessage = {
 export default function MyEvents() {
     return (
       <View flex style={styles.main}>
-        <View flex>
+        <View style={{height: 230, padding: 10}}>
           <Text style={styles.header}>Event Information</Text>
           <BookmarkCard item={TEMP_ITEM} />
         </View>
 
-        <View flex style={styles.section}>
+        <View style={[styles.section, {height: 200}]}>
           <Text style={styles.header}>Message Board</Text>
-          <ScrollView
-           showsVerticalScrollIndicator={false}
-           contentContainerStyle={styles.articles}>
-           <Person person={sampleMessage} />
-          </ScrollView>
+          <Person person={sampleMessage} />
           <View style={styles.inputContainer}>
             <Text>Reply</Text>
             <TextInput
@@ -93,7 +57,6 @@ export default function MyEvents() {
               <Person person={Jack} />
               <Person person={Jack} />
               <Person person={Jack} />
-              <Person person={Jack} />
             </List.Section>
           </ScrollView>
         </View>
@@ -104,7 +67,6 @@ export default function MyEvents() {
 const styles = StyleSheet.create({
   main: {
     justifyContent: 'center',
-    width: width - theme.SIZES.BASE,
   },
   section: {
     borderColor: fidoTheme.COLORS.BORDER,
@@ -115,38 +77,11 @@ const styles = StyleSheet.create({
     fontSize: 16,
     padding: 10,
   },
-  container: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  img: {
-    marginTop: 10,
-    marginRight: 16,
-    marginLeft: 8,
-  },
-  title: {
-    fontSize: 14,
-    paddingTop: 10,
-  },
-  message: {
-    fontSize: 14,
-    paddingTop: 10,
-    color: "grey",
-  },
-  buttonContainer: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "space-between",
-    alignItems: "flex-end",
-    paddingTop: 10,
-  },
   articles: {
     width: width - theme.SIZES.BASE,
   },
   inputContainer: {
-    marginBottom: 12,
-    marginLeft: 12,
-    marginRight: 12
+    margin: 12,
   },
   input: {
     marginTop: 5
