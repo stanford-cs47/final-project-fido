031ccfbff44444fa0f1f1981353a0384e6fc53a2
Irene Han <irenehan@stanford.edu>
2019-12-04 20:04:37 -0800

    people profiles

---
+++ constants/people.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    image: 'https://i.pinimg.com/originals/77/96/41/779641e5e28ec3444cabe66532558f69.jpg',
+    name: 'Prince (Jack Nichols)',
+    icon: true,
+  },
+  {
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwvlVtdAhdmNu-mCCCBOZ06cevhG0n8mMMC4kFcccdA7ZTmKujfw&s',
+    name: 'Spots (James Landay)',
+    icon: true,
+  },
+  {
+    image: 'https://image.cnbcfm.com/api/v1/image/105992231-1561667465295gettyimages-521697453.jpeg?v=1561667497',
+    name: 'Cliff (Zev Burstein)',
+    icon: true,
+  },
+  {
+    image: 'https://www.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg',
+    name: 'Coco (Irene Han)',
+    icon: true,
+  }
+];
